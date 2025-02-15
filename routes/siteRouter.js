8d5965eb1a63f4d9a6315f9de2a40d6213538fad
santisify <2812192494@qq.com>
2025-02-15 20:12:34 +0800

    Update siteRouter.js

--- routes/siteRouter.js
+++ routes/siteRouter.js
@@ -18,19 +18,17 @@ router.get('/', async (req, res) => {
     }
 })
 
-router.put('/put', async (req, res, params) => {
+router.post('/add', async (req, res, params) => {
     try {
         const dbClient = await connectDB();
         console.log(params);
         // const collection = dbClient.db('lazyboy').collection('siteInfo').insertOne(param);
         res.status(200).json({
-            success: true,
-            data: params
+            success: true, data: params
         })
     } catch (err) {
         res.status(404).json({
-            success: false,
-            message: err.message
+            success: false, message: err.message
         })
     }
 })
