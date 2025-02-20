f7d6c7d346664d8d0ae46e844051395849a2603d
santisify <2812192494@qq.com>
2025-02-21 00:06:15 +0800

    Update githubcontributionRouter.js

--- routes/githubcontributionRouter.js
+++ routes/githubcontributionRouter.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const axios = require('axios')
+const axios = require('axios');
 
 router.get('/contributions/:username', async (req, res) => {
-    const {username} = req.params;
+    const { username } = req.params;
     const token = process.env.GITHUB_TOKEN;
 
     try {
-        const repos = await axios.get('https://api.lazy-boy-acmer.cn/img/all');
+        const response = await axios.get(`https://api.github.com/users/${username}/repos`, {
+            headers: {
+                Authorization: token ? `token ${token}` : '', // 如果有 token 就加上，没有就不加
+            },
+        });
+        const repos = response.data;
         res.status(200).json(repos);
     } catch (error) {
         console.error(error);
-        res.status(500).json({error: 'Failed to fetch contributions'});
+        res.status(500).json({ error: 'Failed to fetch contributions' });
     }
 });
 
